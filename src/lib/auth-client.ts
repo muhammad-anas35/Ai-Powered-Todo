a4debb47fae9224a0da486f7c4c086d0e519898b
Muhammad_Anas <m401anas@gmail.com>
2025-12-22 23:58:12 +0500

    production ready

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://ai-focus-todo.vercel.app/"
 })
 
 export const { useSession } = authClient;
